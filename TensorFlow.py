ca561e14e360b141bccb8a386c19f26f3eec4d64
anish_dabhane <anishdabhane@gmail.com>
2023-06-20 11:12:52 +0530

    basic ann

--- TensorFlow.py
+++ TensorFlow.py
@@ -1,6 +1,90 @@
-print("hello")
-print("I am a human")
-a =1
-b=2
-print(a+b)
+import numpy
+import scipy.special
+class neuralNetwork:
+
+
+    # initialise the neural network
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes in each input, hidden, output layer
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        # link weight matrices, wih and who
+        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
+        # w11 w21
+        # w12 w22 etc
+        self.wih = numpy.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
+
+        # learning rate
+        self.lr = learningrate
+        # activation function is the sigmoid function
+        self.activation_function = lambda x: scipy.special.expit(x)
+
+        pass
+
+
+    # train the neural network
+    def train(self, inputs_list, targets_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+
+    def query(self,input_list):
+        #convert inputs list into 2d array
+        inputs = numpy.array(input_list,ndmin=2).T
+        hidden_inputs = numpy.dot(self.wih,inputs)
+        hidden_outputs =self.activation_function(hidden_inputs)
+        final_inputs = numpy.dot(self.who,hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
+
+ # number of input, hidden and output nodes
+input_nodes = 784
+hidden_nodes = 400
+output_nodes = 10
+
+# learning rate
+learning_rate = 0.2
+
+# create instance of neural network
+n = neuralNetwork(input_nodes,hidden_nodes,output_nodes, learning_rate)
+
+# load the mnist training data CSV file into a list
+training_data_file = ('/home/Dataset/mniopenst_train.csv', 'r')
+training_data_list = training_data_file.readlines()
+training_data_file.close()
+
+# train the neural network
+
+# epochs is the number of times the training data set is used for training
+epochs = 8
+
+for e in range(epochs):
+    #go through all records in the training data set
+    for record in training_data_list:
+        #split the record by the ',' commas
+        all_values = record.split(',')
+        #scale and shift the inputs
+        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+        #create the target output values (all 0.01, except the desired label which is 0.99)
+        targets = numpy.zeros(output_nodes) + 0.01
+        #all_values[0] is the target label for this record
+        targets[int(all_values[0])] = 0.99
+        n.train(inputs, targets)
+        pass
+    pass
 
